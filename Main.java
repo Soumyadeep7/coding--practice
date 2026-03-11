efc5eaf35f81109d556d05e75027f87a20697956
Soumyadeep Das <111475097+Soumyadeep7@users.noreply.github.com>
2026-03-11 17:42:37 +0530

    next practice

--- Main.java
+++ Main.java
@@ -1,26 +1,11 @@
-import java.util.HashMap;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        int nums[] = {4,1,2,1,2};
-        int n = nums.length;
-        int res = 0;
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-
-        for(int i = 0; i < n; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-
-        for(int i = 0; i < n; i++){
-            if(map.get(nums[i]) == 1){
-                res = nums[i];
-                break;
-            }
-        }
-
-        System.out.println(res);
-    }
+import java.util.Scanner;
+ public class Main{
+public static void main(String[]Args){
+int nums[]={4,1,2,1,2};
+int XOR=0;
+for(int num:nums){
+XOR=XOR^num;
+}
+System.out.println(XOR);
+}
 }
